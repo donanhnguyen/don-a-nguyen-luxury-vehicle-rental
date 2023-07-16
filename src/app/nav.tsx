cdb4669c47d372fcb1cb27cf3122a5bbad0f59c9
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-07-16 18:07:18 -0500

    small edit

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -6,7 +6,7 @@ export default function Nav () {
              <ul className="flex items-center bg-gray-800 w-screen fixed top-0">
                 <li>
                     <h1 className="italic text-4xl text-white">
-                        <span className="text-orange-500">DON </span>ANH NGUYEN LUXURY VEHICLES
+                        <span className="text-orange-500">L</span>UXURY VEHICLES
                     </h1>
                 </li>
             </ul>
